85a491ebc6391b277cc8afe4c636437234cee456
Kiran Anto <kirananto@gmail.com>
2017-12-29 17:41:49 +0530

    [VUEX] Add getters

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -64,7 +64,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 html, body, .container-table {
   height: 100%;
   background-color: #ECEFF1 !important;
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -80,7 +80,7 @@ export default {
 }
 </script>
 
-<style >
+<style scoped>
 
 html, body, .container-table {
   height: 100%;
