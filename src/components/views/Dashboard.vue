a10b2aa45d3a3074fb90985eab03a09cc705d2b2
Kiran Anto <kirananto@gmail.com>
2017-12-30 15:34:02 +0530

    Remove settings component

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -3,7 +3,7 @@
   <section class="content">
     <!-- Info boxes -->
     <div class="row">
-      <div class="alert alert-success alert-dismissible">
+      <div class="alert alert-error alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         <h4><i class="icon fa fa-check"></i> Welcome to timeVue</h4>
         check it out on github. <a href="https://github.com/kirananto/timeVue" target="_blank"><i class="fa fa-github fa-2x"></i></a>
