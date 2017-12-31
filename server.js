5b2723a712d91c029a819a3f5d8896498c3645e3
Kiran Anto <kirananto@gmail.com>
2017-12-31 15:11:03 +0530

    Add history-api-fallback to work with heroku

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
+
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
