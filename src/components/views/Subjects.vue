6a77df1a885f8181777cb9afd8ed843815722281
Kiran Anto <kirananto@gmail.com>
2018-02-27 13:52:49 +0530

    fix subjects

--- src/components/views/Subjects.vue
+++ src/components/views/Subjects.vue
@@ -10,15 +10,37 @@
 
             <div class="box-body text-center">
               <!-- calendar group -->
+              <h1>SELECT CLASS</h1>
+              
               <div class="vcenter">
+                  
                  <div class="row">
-                    <input v-model="subCode"  type="text" placeholder="Enter Subject ID" >
+                   <!-- {{classList}} -->
+                   <select v-model="selectedClass"  placeholder="Select Class">
+                      <option v-for="(option,key) in classList" :key="key" v-bind:value="option">
+                        {{ option }}
+                      </option>
+                    </select>
+                    <select v-model="selectedBranch"  placeholder="Select Branch">
+                      <option v-for="(option,key) in branches" :key="key" v-bind:value="option">
+                        {{ option }}
+                      </option>
+                    </select>
 
-                    <input v-model="division"  type="text" placeholder="division" >
+                     <select v-model="selecteDivision"  placeholder="Select Class">
+                      <option v-for="(option,key) in divisions" :key="key" v-bind:value="option">
+                        {{ option }}
+                      </option>
+                    </select>
 
-                    <input v-model="tCode" type="text" placeholder="Teacher Code" >
+                    <table>
+                      <thead>
+                        <td>SUBJECT NAME</td>
+                      </thead>
+                    </table>
+                    <!-- <input v-model="tCode" type="text" placeholder="Teacher Code" >
              
-                    <button v-on:click="addSubject" class="btn add-button"><i class="fa fa-plus"></i></button>
+                    <button v-on:click="addSubject" class="btn add-button"><i class="fa fa-plus"></i></button> -->
              
                   </div>
                 <!--   <h1>List of Teachers</h1>
@@ -51,21 +73,52 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import firebase from 'firebase'
+  require('firebase/firestore')
   export default {
     data: function () {
       return {
         subCode: null,
         division: null,
-        tCode: null
+        tCode: null,
+        branches: null,
+        divisions: null,
+        selectedClass: null,
+        selectedBranch: null,
+        selecteDivision: null
       }
     },
     computed: {
       ...mapGetters([
-        'isCurrentClassSet'])
+        'isCurrentClassSet',
+        'classList'])
     },
     methods: {
       addSubject: function () {
         // TODO code for adding subject here
+      }
+    },
+    watch: {
+      selectedClass: function (val) {
+        this.branches = []
+        this.divisions = []
+        firebase.firestore().collection(`classes/${val}/branches`).get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              this.branches.push(doc.id)
+            })
+          })
+      },
+      selectedBranch: function (val) {
+        this.divisions = []
+        const currentbranch = `classes/${this.selectedClass}/branches/${val}/divisions`
+        console.log(currentbranch)
+        firebase.firestore().collection(currentbranch).get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              this.divisions.push(doc.id)
+            })
+          })
       }
     }
   }
@@ -74,7 +127,6 @@
   .vcenter {
     display: inline-block;
     vertical-align: middle;
-    padding-top: 14em;
     color: grey;
     padding-bottom: 18em;
 }
@@ -85,6 +137,35 @@ input[type=text] {
     border-radius: 1rem;
     padding-left: 2rem;
 }
+
+select {
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    -webkit-padding-end: 20px;
+    -moz-padding-end: 20px;
+    -webkit-padding-start: 2px;
+    -moz-padding-start: 2px;
+    background-color: #F0F0F0; /* fallback color if gradients are not supported */
+    background-position: center right;
+    background-repeat: no-repeat;
+    text-transform: uppercase;
+    border: none;
+    border-radius: 2px;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+    color: #555;
+    font-size: inherit;
+    margin: 0;
+    overflow: hidden;
+    padding-top: 2px;
+    padding-bottom: 2px;
+    content:'2';  
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+
 .add-button {
     height: 5rem;
     border-radius: 1rem;
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'es6-promise/auto'
 // Import System requirements
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+// import vSelect from 'vue-select'
 import { sync } from 'vuex-router-sync'
 import routes from './routes'
 import store from './store'
@@ -14,12 +14,13 @@ require('firebase/firestore')
 // Import Views - Top level
 import AppView from './components/App.vue'
 
+// Vue.use(vSelect)
 Vue.use(VueRouter)
 Vue.use(VueParticles)
 
 firebase.initializeApp(store.getters.getFirebaseConfig)
 
-var db = firebase.firestore()
+// var db = firebase.firestore()
 
 let app
 // Routing logic
@@ -33,66 +34,66 @@ var router = new VueRouter({
 })
 
 // Some middleware to help us ensure the user is authenticated.
-router.beforeEach((to, from, next) => {
-  let currentUser = firebase.auth().currentUser
-  if (!store.getters.getUser && currentUser) {
-    currentUser.isAdmin = false
-    store.commit('SET_USER', currentUser)
-  }
-  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  let details = to.matched.some(record => record.meta.details)
-  let requireAdmin = to.matched.some(record => record.meta.requireAdmin)
-  // console.log(to.fullPath)
-  if (requiresAuth && !currentUser) {
-    next({
-      path: '/login',
-      query: {redirect: to.fullPath}
-    })
-  } else if (requiresAuth && currentUser) {
-    if (requireAdmin) {
-      if (store.getters.getUser.isAdmin) {
-        next()
-      } else {
-        next({
-          path: '/accessdenied'
-        })
-      }
-    } else {
-      db.collection('users').doc(currentUser.uid).get()
-      .then((doc) => {
-        if (doc.exists) {
-          // console.log(to.fullPath)
-          // console.log(store.getters.isisCurrentClassSet)
-          if (to.fullPath === '/details') {
-            next('/profile')
-          } else if (to.fullPath === '/' && store.getters.isCurrentClassSet) {
-            next('/timetable')
-          } else if (to.fullPath === '/timetable' && !store.getters.isCurrentClassSet) {
-            next('/')
-          } else {
-            next()
-          }
-        } else {
-          if (!details) {
-            console.log(currentUser.uid + '--' + doc.exists)
-            next({
-              path: '/details'
-            })
-          } else {
-            next()
-          }
-        }
-      })
-    }
-  } else {
-    if (currentUser && to.fullPath === '/login') {
-      console.log('Already Logged In')
-      next('/')
-    } else {
-      next()
-    }
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   let currentUser = firebase.auth().currentUser
+//   if (!store.getters.getUser && currentUser) {
+//     currentUser.isAdmin = false
+//     store.commit('SET_USER', currentUser)
+//   }
+//   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+//   let details = to.matched.some(record => record.meta.details)
+//   let requireAdmin = to.matched.some(record => record.meta.requireAdmin)
+//   // console.log(to.fullPath)
+//   if (requiresAuth && !currentUser) {
+//     next({
+//       path: '/login',
+//       query: {redirect: to.fullPath}
+//     })
+//   } else if (requiresAuth && currentUser) {
+//     if (requireAdmin) {
+//       if (store.getters.getUser.isAdmin) {
+//         next()
+//       } else {
+//         next({
+//           path: '/accessdenied'
+//         })
+//       }
+//     } else {
+//       db.collection('users').doc(currentUser.uid).get()
+//       .then((doc) => {
+//         if (doc.exists) {
+//           // console.log(to.fullPath)
+//           // console.log(store.getters.isisCurrentClassSet)
+//           if (to.fullPath === '/details') {
+//             next('/profile')
+//           } else if (to.fullPath === '/' && store.getters.isCurrentClassSet) {
+//             next('/timetable')
+//           } else if (to.fullPath === '/timetable' && !store.getters.isCurrentClassSet) {
+//             next('/')
+//           } else {
+//             next()
+//           }
+//         } else {
+//           if (!details) {
+//             console.log(currentUser.uid + '--' + doc.exists)
+//             next({
+//               path: '/details'
+//             })
+//           } else {
+//             next()
+//           }
+//         }
+//       })
+//     }
+//   } else {
+//     if (currentUser && to.fullPath === '/login') {
+//       console.log('Already Logged In')
+//       next('/')
+//     } else {
+//       next()
+//     }
+//   }
+// })
 
 sync(store, router)
 // Start out app!
