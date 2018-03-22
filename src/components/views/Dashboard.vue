b958545ce60f49594d0ee97559097b4b19286abb
Kiran Anto <kirananto@gmail.com>
2018-03-22 12:00:59 +0530

    Avoid non-primitive keys

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -3,7 +3,7 @@
   <section class="content">
     <!-- Info boxes -->
     <div class="row">
-      <div v-for="(key,item) in getNotifications" class="alert alert-dismissible" :key="key" :class="item.type">
+      <div v-for="(item, key) in getNotifications" class="alert alert-dismissible" :key="key" :class="item.type">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         <h4><i class="icon fa" :class="item.icon"></i>{{item.title}}</h4>
         <span v-html="item.body"></span>
